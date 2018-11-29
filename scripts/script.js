460b8b8cc2c325f4c68a8e35c1ef26708bbd8d98
Ltricha <34155601+Ltricha@users.noreply.github.com>
2018-11-29 17:13:01 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
-img = document.getElementsByClassName("img");
+
+var i;
+var menuButton = document.getElementsByClassName("menu-button");
+var navItem = document.getElementsByClassName("nav-item");
+
+console.log(menuButton.length);
 
 
-for(var i = 0; i < img.length; i++){
-	img[i].addEventListener("click", function(){
-		alert("test");
-	})
+for(i = 0; i < menuButton.length; i++){
+	menuButton[0].addEventListener("click", function(){
+		toggleClass(navItem, "show");
+	});
+}
+
+
+
+function toggleClass(elementName, className){
+	console.log(elementName);
+
+	for(i = 0; i < elementName.length; i++){
+		elementName[i].classList.toggle(className);
+		console.log("hi");
+	}
+
 }
