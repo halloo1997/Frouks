bd3ea2f789d6c75fced9c93899d0892638dea871
Ltricha <34155601+Ltricha@users.noreply.github.com>
2018-11-29 20:53:23 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ var i;
 var menuButton = document.getElementsByClassName("menu-button");
 var navItem = document.getElementsByClassName("nav-item");
 
-console.log(menuButton.length);
 
 
 for(i = 0; i < menuButton.length; i++){
@@ -15,11 +14,9 @@ for(i = 0; i < menuButton.length; i++){
 
 
 function toggleClass(elementName, className){
-	console.log(elementName);
 
 	for(i = 0; i < elementName.length; i++){
 		elementName[i].classList.toggle(className);
-		console.log("hi");
 	}
 
 }
