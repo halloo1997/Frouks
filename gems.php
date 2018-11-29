3af879c1f899e9e6032c2544fa8f5e4c9e198d68
Chariesa Roozendaal <chariesa@hotmail.com>
2018-11-29 13:40:26 +0100

    Update gems.php

--- gems.php
+++ gems.php
@@ -1,1 +1,56 @@
-
+<!DOCTYPE html><!--HEADER-->
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="styles/style.css">
+		<link rel="stylesheet" href="styles/responsive.css">
+	</head>
+	<body>
+		<header class="main-header">
+			<div class="search"></div>
+			<a href="#"><img class="logo" src="images/frouks_logo.png"></a><!--Link naar de homepage-->
+			<nav class="top-nav">
+				<div></div>
+				<ul>
+					<li class="nav-item active-link"><a href="#">Collectie</a></li>
+					<li class="nav-item"><a href="#">Workshops</a></li>
+					<li class="nav-item"><a href="#">Stenen Overzicht</a></li>
+					<li class="nav-item"><a href="#">Over Frouks</a></li>
+					<li class="nav-item"><a href="#">Locatie</a></li>
+					<li class="nav-item"><a href="#">Contact</a></li>
+					<li class="nav-item"><img src="images/flag.png"></li>
+				</ul>
+			</nav>
+		</header><!--END HEADER-->
+		<main>
+			<section>
+				<div class="modaal"></div>
+				<h1>Frouks Collectie</h1>
+				<div class="grid-container grid-columns-three">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+					<img class="thumbnail" src="img.jpg">
+				</div>
+			</section>
+		</main>
+		<script src="scripts/script.js"></script>
+	</body>
+</html>
