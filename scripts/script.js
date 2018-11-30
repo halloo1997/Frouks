81a29188c18ce1c05c90cd784c78a94d36092fe6
Ltricha <34155601+Ltricha@users.noreply.github.com>
2018-11-30 23:13:40 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,41 @@
 
 var i;
+
+
+
+var topNav = document.getElementsByClassName("top-nav");
+
+
+
+
+window.addEventListener("scroll", function(){
+	if(window.pageYOffset >= topNav[0].offsetTop){
+		topNav[0].classList.add("sticky");
+	} else{
+		topNav[0].classList.remove("sticky");
+	}
+})
+
+
+
+
+window.addEventListener("click", function(ev){
+	console.log(ev.target);
+})
+
+
+
+
+
+
+
 var menuButton = document.getElementsByClassName("menu-button");
 var navItem = document.getElementsByClassName("nav-item");
 
 
-var topNav = document.getElementsByClassName("nav-top);
-
-for(i = 0; i < topNav.length; i++){
-	
-}
-
-
-for(i = 0; i < menuButton.length; i++){
-	menuButton[0].addEventListener("click", function(){
-		toggleClass(navItem, "show");
-	});
-}
+menuButton[0].addEventListener("click", function(){
+	toggleClass(navItem, "show");
+});
 
 
 
